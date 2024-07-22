c2fd441d8d5a5954dda88c2686469c04bc67668b
lokscheng <81004424just@gmail.com>
2024-07-22 11:07:45 +0100

    Add files via upload

---
+++ words_year1.js
@@ -0,0 +1,6 @@
+const words = [
+    { day: '2024-07-16', words: ["the", "that", "not", "look", "put", "could"] },
+    { day: '2024-07-17', words: ["and", "with", "then", "don't", "come", "house"] },
+    // Add more words for Year R
+];
+
---
+++ words_year2.js
@@ -0,0 +1,6 @@
+const words = [
+    { day: '2024-07-12', words: ["heptagon", "octagon", "monitor", "tadpoles", "child"] },
+    { day: '2024-07-13', words: ["seize", "automatic", "consider", "favourite", "continue"] },
+    // Add more words for Year 1
+];
+
